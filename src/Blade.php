d0aa74bb7b116b8518eb0b9a6d4edb8dc179bd7d
LZL0 <chief.linkyou@hotmail.com>
2021-09-30 03:02:20 +0200

    Attempt to resolve PHP warning.

--- src/Blade.php
+++ src/Blade.php
@@ -136,7 +136,7 @@ class Blade implements FactoryContract
         $this->container->singleton(Application::class, function () {
             return new class {
                 public function getNamespace() {
-                    $composer = json_decode(file_get_contents('composer.json'), true);
+                    $composer = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
                     return count($composer['autoload']['psr-4'] ?? []) ? array_keys($composer['autoload']['psr-4'])[0] : null;
                 }
             };
